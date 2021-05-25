218e4dc7938bab39f90e2374fbcfc53fe9db0d1f
motform <love.lagerkvist@protonmail.com>
2021-05-25 13:36:34 +0200

    Remove `live` flag from exhibitionPage frontmatter

--- src/templates/exhibition-page.js
+++ src/templates/exhibition-page.js
@@ -8,41 +8,8 @@ import ExhibitionGrid from '../components/ExhibitionGrid';
 /* An exhibition page features a header with an introduction and then a grid of projects.
    These projects do not have their own pages. If a 'visitor' wants to see a project, it has
    to do so via a link to a student's Zoom. Very 2014-ephemeral.
- 
+   
    — LLA 200528 */
-
-class ExhibitionPageHeadLive extends React.Component {
-  render() {
-    return (
-      <div className="container content">
-        <h1>{this.props.exhibition.frontmatter.title}</h1>
-        <div className="section tile is-ancestor">
-          <div className="is-parent is-6" >
-            <div className="tile is-child" dangerouslySetInnerHTML={{ __html: this.props.exhibition.html }} />
-          </div>
-          <div className="tile is-6 is-vertical" >
-            <div className="tile">
-              <div className="tile is-child is-4" >  </div>
-              <div className="tile is-child is-8 box" >
-                <p className="is-small" >
-                  To visit a student, check their avalible times and click the zoom button! There are links to student's Miro boards, some of which require you to be logged in. A free account should be enough, if that is the case. 
-                </p>
-
-                <div className="buttons" >
-                  <button className="button is-small" title="Zoom" disabled>Zoom</button>
-                  <button className="button is-small" title="Miro" disabled>Miro</button>
-                </div>
-                <p className="is-small has-text-weight-semibold" >
-                </p>
-              </div>
-            </div>
-            <div className="tile is-child" ></div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
 
 class ExhibitionPageHead extends React.Component {
   render() {
@@ -63,15 +30,9 @@ class ExhibitionPage extends React.Component {
   render() {
     const exhibition = this.props.data.exhibition;
     const projects = this.props.data.projects.edges;
+    const head = <ExhibitionPageHead exhibition={exhibition} />;
 
-    console.log(exhibition);
-    let head;
-    if (exhibition.frontmatter.live)
-      head = <ExhibitionPageHeadLive exhibition={exhibition} />
-    else
-      head =<ExhibitionPageHead exhibition={exhibition} />
-
-      return (
+    return (
       <Layout>
         <section className="section">
           <Helmet title={`Arkixd. ${exhibition.frontmatter.title}`} />
@@ -94,7 +55,6 @@ query ExhibitionQuery($id: String!) {
       year
       course
       title
-      live
     }
   }
   projects: allMarkdownRemark(sort: {order: ASC, fields: [frontmatter___priority]}, filter: {frontmatter: {templateKey: {eq: "exhibition-post"}}}) {
